bfba9a645941fbe858d9622d4fb804187cb3610f
jbottelb <jbottelb@nd.edu>
2021-04-11 18:10:26 -0400

    inital commit

---
+++ api.py
@@ -0,0 +1,17 @@
+import flask
+from flask import request
+
+import pandas as pd
+
+data = pd.read_csv('data.csv', names=['name', 'id', 'thing']).set_index('id')
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=["get"])
+def home():
+    try:
+        key = int(request.args['id'])
+
+        return "I thought of an example and it was a " + key['thing']
+    except KeyError:
+        return "Bad"
